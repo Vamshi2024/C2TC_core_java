0c94dc86fcd246fdf49cdaaf9047e79457372197
theva <theva@vamshi>
2023-10-07 19:49:29 +0530

    first commit

---
+++ src/neww/demo.java
@@ -0,0 +1,20 @@
+package neww;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		++a;
+		int c=++a +b+ a--;
+		
+		System.out.println(c);
+		System.out.println(b);
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/neww/forloop.java
@@ -0,0 +1,16 @@
+package neww;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for (int i=1;i<=10;i++) 
+			System.out.println("value of i: ");
+			System.out.println(i);
+		
+		
+
+	}
+
+}
